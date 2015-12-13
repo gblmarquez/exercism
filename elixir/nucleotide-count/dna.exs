8a06a33cdebccc77c8913bf2b793fbf7961b32b4
Gabriel Marquez <gabriel@conube.com.br>
2015-12-13 01:06:00 -0200

    Add nucleotide-count implementation

--- elixir/nucleotide-count/dna.exs
+++ elixir/nucleotide-count/dna.exs
@@ -14,7 +14,16 @@ defmodule DNA do
   """
   @spec count([char], char) :: non_neg_integer
   def count(strand, nucleotide) do
+    if Enum.any?([?A, ?C, ?G, ?T], fn(x)-> x === nucleotide end) === false do
+      raise ArgumentError
+    end
 
+    if Enum.all?(strand, fn(x)-> Enum.any?([?A, ?C, ?G, ?T], fn(y)-> y === x end) end) === false do
+      raise ArgumentError
+    end
+
+    strand
+      |> Enum.count(fn(x) -> x == nucleotide end)
   end
 
 
@@ -28,6 +37,9 @@ defmodule DNA do
   """
   @spec nucleotide_counts([char]) :: Dict.t
   def nucleotide_counts(strand) do
-
+    %{?A => count(strand, ?A),
+      ?C => count(strand, ?C),
+      ?G => count(strand, ?G),
+      ?T => count(strand, ?T)}
   end
 end
