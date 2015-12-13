40bd4fcdbdb22b2c90b97165c63769cd8b9379fd
Gabriel Marquez <gabriel@conube.com.br>
2015-12-13 00:30:02 -0200

    Add anagram implementation

--- elixir/anagram/anagram.exs
+++ elixir/anagram/anagram.exs
@@ -4,6 +4,18 @@ defmodule Anagram do
   """
   @spec match(String.t, [String.t]) :: [String.t]
   def match(base, candidates) do
-  
+    base_count = String.length(base)
+    base_chars = String.codepoints(String.downcase(base))
+
+    candidates
+      |> Enum.filter(fn(x) -> String.length(x) == base_count end)
+      |> Enum.filter(fn(x) -> anagram(x, base_chars) end)
+      |> Enum.uniq
+  end
+
+  defp anagram(word, source_chars) do
+    word_chars = String.codepoints(String.downcase(word))
+    source_chars !== word_chars
+      && Enum.sort(word_chars) === Enum.sort(source_chars)
   end
 end
