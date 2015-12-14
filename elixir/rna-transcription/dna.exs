1b5c672138443178f90f99eebe650a05ecd09e95
Gabriel Marquez <gabriel@conube.com.br>
2015-12-14 10:02:38 -0200

    Add rna-transcription implementation

--- elixir/rna-transcription/dna.exs
+++ elixir/rna-transcription/dna.exs
@@ -9,6 +9,15 @@ defmodule DNA do
   """
   @spec to_rna([char]) :: [char]
   def to_rna(dna) do
-
+    Enum.map(dna, fn(c) ->
+      cond do
+        c == ?A -> 'U'
+        c == ?C -> 'G'
+        c == ?T -> 'A'
+        c == ?G -> 'C'
+      end
+    end)
+    |> Enum.join
+    |> String.to_char_list
   end
 end
--- elixir/rna-transcription/rna_transcription_test.exs
+++ elixir/rna-transcription/rna_transcription_test.exs
@@ -15,22 +15,22 @@ defmodule DNATest do
     assert DNA.to_rna('G') == 'C'
   end
 
-  @tag :pending
+  # @tag :pending
   test "transcribes cytosine to guanine" do
     assert DNA.to_rna('C') == 'G'
   end
 
-  @tag :pending
+  # @tag :pending
   test "transcribes thymidine to adenine" do
     assert DNA.to_rna('T') == 'A'
   end
 
-  @tag :pending
+  # @tag :pending
   test "transcribes adenine to uracil" do
     assert DNA.to_rna('A') == 'U'
   end
 
-  @tag :pending
+  # @tag :pending
   test "it transcribes all dna nucleotides to rna equivalents" do
     assert DNA.to_rna('ACGTGGTCTTAA') == 'UGCACCAGAAUU'
   end
