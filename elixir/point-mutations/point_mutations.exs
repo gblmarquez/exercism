3b7eb9c54c3dac6038375990813c0dd9d0470d2c
Gabriel Marquez <gabriel@conube.com.br>
2016-03-09 21:04:15 -0300

    Implements hamming distance

--- elixir/point-mutations/point_mutations.exs
+++ elixir/point-mutations/point_mutations.exs
@@ -9,5 +9,21 @@ defmodule DNA do
   """
   @spec hamming_distance([char], [char]) :: non_neg_integer
   def hamming_distance(strand1, strand2) do
+    cond do
+      strand1 === strand2 ->
+        0
+      Enum.count(strand1) !== Enum.count(strand2) ->
+        nil
+      true ->
+        strand1
+        |> Enum.with_index
+        |> Enum.map(fn(x) ->
+          index = elem(x, 1)
+          if Enum.at(strand1, index) == Enum.at(strand2, index),
+            do: 0,
+          else: 1
+        end)
+        |> Enum.sum
+    end
   end
 end
